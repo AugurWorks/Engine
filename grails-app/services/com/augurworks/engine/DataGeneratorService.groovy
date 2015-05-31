ec98a6c6552bf368aea33385dec6f148fcb632c8
TheConnMan <bcconn2112@gmail.com>
2015-05-31 14:02:44 -0400

    Cleaned up Quandl data importer.

--- grails-app/services/com/augurworks/engine/DataGeneratorService.groovy
+++ grails-app/services/com/augurworks/engine/DataGeneratorService.groovy
@@ -6,15 +6,10 @@ import grails.transaction.Transactional
 class DataGeneratorService {
 
 	void importQuandlDataSets() {
-		boolean first = true;
-		new URL('https://s3.amazonaws.com/quandl-static-content/quandl-stock-code-list.csv').getText().eachLine { String line ->
-			if (first) {
-				first = false;
-			} else {
-				Collection<String> row = line.split(',');
-				if (row[2] != 'NA' && row[4] == 'Active') {
-					new DataSet(ticker: row[0], name: row[1], code: row[2], dataColumn: 4).save();
-				}
+		new URL('https://s3.amazonaws.com/quandl-static-content/quandl-stock-code-list.csv').getText().split('\n').tail().each { String line ->
+			Collection<String> row = line.split(',');
+			if (row[2] != 'NA' && row[4] == 'Active') {
+				new DataSet(ticker: row[0], name: row[1], code: row[2], dataColumn: 4).save();
 			}
 		}
 	}
