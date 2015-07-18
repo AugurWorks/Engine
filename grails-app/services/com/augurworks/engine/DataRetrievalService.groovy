e4b72e7d3a4bcae0e3ff40e8a12f7a2e77efff5b
TheConnMan <bcconn2112@gmail.com>
2015-07-18 15:26:34 -0400

    Refactored getDataSetValues to get expanded value range.

--- grails-app/services/com/augurworks/engine/DataRetrievalService.groovy
+++ grails-app/services/com/augurworks/engine/DataRetrievalService.groovy
@@ -9,27 +9,26 @@ class DataRetrievalService {
 	def grailsApplication
 
 	Collection<Map> getRequestValues(AlgorithmRequest algorithmRequest) {
+		int minOffset = algorithmRequest.requestDataSets*.offset.min()
+		int maxOffset = algorithmRequest.requestDataSets*.offset.max()
 		GParsPool.withPool(algorithmRequest.requestDataSets.size()) {
 			return algorithmRequest.requestDataSets.collectParallel { RequestDataSet requestDataSet ->
 				return [
 					name: requestDataSet.dataSet.name,
-					values: getDataSetValues(requestDataSet.dataSet, algorithmRequest.startDate, algorithmRequest.endDate, requestDataSet.offset)
+					values: getDataSetValues(requestDataSet.dataSet, algorithmRequest.startDate, algorithmRequest.endDate, minOffset, maxOffset)
 				];
 			}
 		}
 	}
 
-	Collection getDataSetValues(DataSet dataSet, Date startDate, Date endDate, int offset) {
-		Collection rawData = getQuandlData(dataSet.code, dataSet.dataColumn);
-		int startIndex = rawData.findIndexOf { it[0] == startDate.format('yyyy-MM-dd') };
-		int endIndex = rawData.findIndexOf { it[0] == endDate.format('yyyy-MM-dd') };
-		if (startIndex != -1 && endIndex != -1 && startIndex + offset >= 0 && endIndex <= rawData.size()) {
-			return rawData[(startIndex + offset)..(endIndex + offset)];
-		} else {
-			String dateFormat = grailsApplication.config.augurworks.dateFormat;
-			log.warn 'Invalid date range for ' + dataSet.name + ': ' + startDate.format(dateFormat) + '-' + endDate.format(dateFormat)
-			return [];
+	Collection getDataSetValues(DataSet dataSet, Date startDate, Date endDate, int minOffset, int maxOffset) {
+		Collection rawData = getQuandlData(dataSet.code, dataSet.dataColumn)
+		int startIndex = rawData.findIndexOf { it[0] == startDate.format('yyyy-MM-dd') }
+		int endIndex = rawData.findIndexOf { it[0] == endDate.format('yyyy-MM-dd') }
+		if (startIndex == -1 || endIndex == -1 || startIndex + minOffset < 0 || endIndex + maxOffset > rawData.size() - 1) {
+			throw new AugurWorksException(dataSet.name + ' does not contain data for the requested range ')
 		}
+		return rawData[(startIndex + minOffset)..(endIndex + maxOffset)]
 	}
 
 	Collection getQuandlData(String quandlCode, int dataColumn) {
