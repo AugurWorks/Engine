32a9e294838ebf7e723f4206da8262c26298e6d4
TheConnMan <bcconn2112@gmail.com>
2015-07-19 09:10:04 -0400

    Added AlgorithmRequest spec.

--- test/unit/com/augurworks/engine/AlgorithmRequestSpec.groovy
+++ test/unit/com/augurworks/engine/AlgorithmRequestSpec.groovy
@@ -3,18 +3,59 @@ package com.augurworks.engine
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
- */
 @TestFor(AlgorithmRequest)
+@Mock([DataSet, RequestDataSet])
 class AlgorithmRequestSpec extends Specification {
 
-    def setup() {
-    }
+	static final String DATE_FORMAT = 'yyyy-MM-dd'
 
-    def cleanup() {
-    }
+	Map dataSetParams(int num) {
+		return [
+			ticker: 'XX' + num + 'XX',
+			name: 'Stock ' + num,
+			code: 'xxxx',
+			dataColumn: 4
+		]
+	}
 
-    void "test something"() {
-    }
+	Map requestDataSetParams(int num) {
+		return [
+			dataSet: DataSet.get(num),
+			offset: num
+		]
+	}
+
+	AlgorithmRequest validAlgorithmRequest(Collection<Integer> requestDataSetRange) {
+		Date startDate = Date.parse(DATE_FORMAT, '2014-01-01')
+		Date endDate = Date.parse(DATE_FORMAT, '2015-01-01')
+		Date dateCreated = Date.parse(DATE_FORMAT, '2015-01-01')
+		Collection<DataSet> dataSets = requestDataSetRange.collect { int id ->
+			return new DataSet(dataSetParams(id)).save()
+		}
+		AlgorithmRequest algorithmRequest = new AlgorithmRequest(
+			startDate: startDate,
+			endDate: endDate,
+			dateCreated: dateCreated,
+			dependantDataSet: dataSets.first()
+		)
+		algorithmRequest.save()
+		dataSets.each { DataSet dataSet ->
+			new RequestDataSet(
+				dataSet: dataSet,
+				offset: 0,
+				algorithmRequest: algorithmRequest
+			).save()
+		}
+		return algorithmRequest
+	}
+
+	void "test create algorithm request"() {
+		when:
+		AlgorithmRequest algorithmRequest = validAlgorithmRequest((0..3))
+
+		then:
+		algorithmRequest.id == 1
+		AlgorithmRequest.count() == 1
+		algorithmRequest.requestDataSets.size() == 4
+	}
 }
