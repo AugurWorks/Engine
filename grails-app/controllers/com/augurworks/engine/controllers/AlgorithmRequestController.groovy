f37bf9ffc4b25275034fde158becfd510c8689bf
TheConnMan <bcconn2112@gmail.com>
2015-08-08 19:11:34 -0400

    Added try/catches to AlgorithmRequest submittal functions.

--- grails-app/controllers/com/augurworks/engine/controllers/AlgorithmRequestController.groovy
+++ grails-app/controllers/com/augurworks/engine/controllers/AlgorithmRequestController.groovy
@@ -2,6 +2,7 @@ package com.augurworks.engine.controllers
 
 import grails.converters.JSON
 
+import com.augurworks.engine.AugurWorksException
 import com.augurworks.engine.domains.AlgorithmRequest
 import com.augurworks.engine.domains.AlgorithmResult
 import com.augurworks.engine.domains.DataSet
@@ -33,25 +34,30 @@ class AlgorithmRequestController {
 	}
 
 	def submitRequest(String startDate, String endDate) {
-		Collection<Map> dataSets = JSON.parse(params.dataSets)
-		Map dependantDataSetMap = dataSets.grep { it.dependant }.first()
-		Map parameters = [
-			startDate: Date.parse(Global.FORM_DATE_FORMAT, startDate),
-			endDate: Date.parse(Global.FORM_DATE_FORMAT, endDate),
-			dependantDataSet: DataSet.findByTicker(dependantDataSetMap.name.split(' - ')[0])
-		]
-		AlgorithmRequest algorithmRequest = new AlgorithmRequest(parameters).save()
-		algorithmRequest.updateDataSets(dataSets)
-		render([success: true, id: algorithmRequest.id] as JSON)
+		try {
+			Collection<Map> dataSets = JSON.parse(params.dataSets)
+			Map dependantDataSetMap = dataSets.grep { it.dependant }.first()
+			Map parameters = [
+				startDate: Date.parse(Global.FORM_DATE_FORMAT, startDate),
+				endDate: Date.parse(Global.FORM_DATE_FORMAT, endDate),
+				dependantDataSet: DataSet.findByTicker(dependantDataSetMap.name.split(' - ')[0])
+			]
+			AlgorithmRequest algorithmRequest = new AlgorithmRequest(parameters).save()
+			algorithmRequest.updateDataSets(dataSets)
+			render([ok: true, id: algorithmRequest.id] as JSON)
+		} catch (e) {
+			log.error e.getMessage()
+			render([ok: false, error: e.getMessage()] as JSON)
+		}
 	}
 
 	def deleteRequest(AlgorithmRequest algorithmRequest) {
 		try {
 			algorithmRequest.delete(flush: true)
-			render([success: true] as JSON)
+			render([ok: true] as JSON)
 		} catch(e) {
 			log.error e.message
-			render([success: false] as JSON)
+			render([ok: false] as JSON)
 		}
 	}
 }
