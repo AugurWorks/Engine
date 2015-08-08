e0fdce42f70d5fc791ecd8262002d2ed60b27d0b
TheConnMan <bcconn2112@gmail.com>
2015-08-08 13:03:14 -0400

    Added Aggregation helper functions.

--- src/groovy/com/augurworks/engine/helper/Aggregation.groovy
+++ src/groovy/com/augurworks/engine/helper/Aggregation.groovy
@@ -1,7 +1,28 @@
 package com.augurworks.engine.helper
 
+import com.augurworks.engine.AugurWorksException
+
 class Aggregation {
 
 	static final Collection<String> TYPES = ['Value', 'Period Change', 'Period Percent Change']
 
+	static Double aggregate(String type, Double previousValue, double currentValue) {
+		switch(type) {
+			case 'Value':
+				return currentValue
+			case 'Period Change':
+				return previousValue != null ? currentValue - previousValue : null
+			case 'Period Percent Change':
+				return previousValue != null ? periodPercentChange(previousValue, currentValue) : null
+			default:
+				throw new AugurWorksException('Invalid aggregation type: ' + type)
+		}
+	}
+
+	static double periodPercentChange(double previousValue, double currentValue) {
+		if (previousValue == 0) {
+			throw new AugurWorksException('Invalid previous value for period percentage change: 0')
+		}
+		return 100 * (currentValue - previousValue) / previousValue
+	}
 }
--- test/unit/com/augurworks/engine/helper/AggregationSpec.groovy
+++ test/unit/com/augurworks/engine/helper/AggregationSpec.groovy
@@ -3,6 +3,29 @@ package com.augurworks.engine.helper
 import grails.test.mixin.*
 import spock.lang.Specification
 
+import com.augurworks.engine.AugurWorksException
+
 class AggregationSpec extends Specification {
 
+	void "test valid period percent change"() {
+		when:
+		double result = Aggregation.periodPercentChange(previousValue, currentValue)
+
+		then:
+		Math.abs(expected - result) < 1
+
+		where:
+		previousValue | currentValue | expected
+		1D            | 2D           | 100D
+		2D            | 1D           | -50D
+		5D            | 0D           | -100D
+	}
+
+	void "test invalid period percent change"() {
+		when:
+		Aggregation.periodPercentChange(0, 100)
+
+		then:
+		thrown(AugurWorksException)
+	}
 }
