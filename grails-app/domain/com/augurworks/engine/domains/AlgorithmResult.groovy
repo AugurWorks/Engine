9936972d40b24a16c3a5e238c782fe7a398237fd
TheConnMan <bcconn2112@gmail.com>
2015-09-27 14:01:44 -0400

    Added tomorrow's value helper function on AlgorithmResult. Added send to Slack function for PredictedValues for after algorithm runs.

--- grails-app/domain/com/augurworks/engine/domains/AlgorithmResult.groovy
+++ grails-app/domain/com/augurworks/engine/domains/AlgorithmResult.groovy
@@ -1,5 +1,9 @@
 package com.augurworks.engine.domains
 
+import groovy.time.TimeCategory
+
+import org.apache.commons.lang.time.DateUtils
+
 class AlgorithmResult {
 
 	Date dateCreated
@@ -18,5 +22,16 @@ class AlgorithmResult {
 
 	static mapping = {
 		predictedValues cascade: 'all-delete-orphan'
+	}
+
+	PredictedValue getTomorrowsValue() {
+		Date tomorrow = DateUtils.ceiling(new Date(), Calendar.DATE)
+		Collection<PredictedValue> filtered = this.predictedValues.grep { PredictedValue value ->
+			value.date == tomorrow
+		}
+		if (filtered.size() == 1) {
+			return filtered.first()
+		}
+		return null
 	}
 }
--- test/unit/com/augurworks/engine/domains/AlgorithmResultSpec.groovy
+++ test/unit/com/augurworks/engine/domains/AlgorithmResultSpec.groovy
@@ -1,9 +1,40 @@
 package com.augurworks.engine.domains
 
+import grails.buildtestdata.mixin.Build
 import grails.test.mixin.*
+import groovy.time.TimeCategory
+
+import org.apache.commons.lang.time.DateUtils
+
 import spock.lang.Specification
 
+@Mock([AlgorithmResult, PredictedValue])
+@Build([AlgorithmResult, PredictedValue])
 @TestFor(AlgorithmResult)
 class AlgorithmResultSpec extends Specification {
 
+	void "test valid get tomorrows value"() {
+		given:
+		AlgorithmResult result = AlgorithmResult.build()
+		PredictedValue.build(algorithmResult: result, date: DateUtils.truncate(new Date(), Calendar.DATE))
+		PredictedValue tomorrow = PredictedValue.build(algorithmResult: result, date: DateUtils.ceiling(new Date(), Calendar.DATE))
+
+		when:
+		PredictedValue actual = result.tomorrowsValue
+
+		then:
+		actual == tomorrow
+	}
+
+	void "test invalid get tomorrows value"() {
+		given:
+		AlgorithmResult result = AlgorithmResult.build()
+		PredictedValue.build(algorithmResult: result, date: DateUtils.truncate(new Date(), Calendar.DATE))
+
+		when:
+		PredictedValue actual = result.tomorrowsValue
+
+		then:
+		actual == null
+	}
 }
