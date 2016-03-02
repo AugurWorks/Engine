68403c1f47c3d99966fd4725c3f9610b839938fb
TheConnMan <bcconn2112@gmail.com>
2016-03-02 18:04:05 -0500

    Add additional hourly predicted set tests

--- test/unit/com/augurworks/engine/domains/AlgorithmResultSpec.groovy
+++ test/unit/com/augurworks/engine/domains/AlgorithmResultSpec.groovy
@@ -8,12 +8,12 @@ import org.apache.commons.lang.time.DateUtils
 
 import spock.lang.Specification
 
-@Mock([AlgorithmResult, PredictedValue])
-@Build([AlgorithmResult, PredictedValue])
+@Mock([AlgorithmRequest, AlgorithmResult, PredictedValue])
+@Build([AlgorithmRequest, AlgorithmResult, PredictedValue])
 @TestFor(AlgorithmResult)
 class AlgorithmResultSpec extends Specification {
 
-	void "test valid get tomorrows value"() {
+	void "test valid get tomorrows value (day)"() {
 		given:
 		AlgorithmResult result = AlgorithmResult.build()
 		PredictedValue.build(algorithmResult: result, date: DateUtils.truncate(new Date(), Calendar.DATE))
@@ -27,10 +27,37 @@ class AlgorithmResultSpec extends Specification {
 		actual == tomorrow
 	}
 
+	void "test valid get tomorrows value (hour)"() {
+		given:
+		AlgorithmRequest algorithmRequest = AlgorithmRequest.build(unit: 'Hour')
+		AlgorithmResult result = AlgorithmResult.build(algorithmRequest: algorithmRequest, endDate: new Date())
+		PredictedValue nextHour = PredictedValue.build(algorithmResult: result, date: DateUtils.ceiling(new Date(), Calendar.HOUR))
+		PredictedValue.build(algorithmResult: result, date: DateUtils.truncate(new Date(), Calendar.HOUR))
+
+		when:
+		PredictedValue actual = result.futureValue
+
+		then:
+		actual == nextHour
+	}
+
 	void "test invalid get tomorrows value"() {
 		given:
-		AlgorithmResult result = AlgorithmResult.build()
+		AlgorithmResult result = AlgorithmResult.build(endDate: new Date())
 		PredictedValue.build(algorithmResult: result, date: DateUtils.truncate(new Date(), Calendar.DATE))
+
+		when:
+		PredictedValue actual = result.futureValue
+
+		then:
+		actual == null
+	}
+
+	void "test invalid get tomorrows value (hour)"() {
+		given:
+		AlgorithmRequest algorithmRequest = AlgorithmRequest.build(unit: 'Hour')
+		AlgorithmResult result = AlgorithmResult.build(algorithmRequest: algorithmRequest, endDate: new Date())
+		PredictedValue.build(algorithmResult: result, date: DateUtils.truncate(new Date(), Calendar.HOUR))
 
 		when:
 		PredictedValue actual = result.futureValue
