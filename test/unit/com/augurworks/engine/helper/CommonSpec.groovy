e0b7143e15cbe7eb05dcc558b78a11d4dbfc31ab
TheConnMan <bcconn2112@gmail.com>
2016-03-03 21:04:54 -0500

    Remove deprecated unit test

--- test/unit/com/augurworks/engine/helper/CommonSpec.groovy
+++ test/unit/com/augurworks/engine/helper/CommonSpec.groovy
@@ -5,25 +5,6 @@ import spock.lang.Specification
 
 class CommonSpec extends Specification {
 
-	void "test add date"() {
-		given:
-		Date start = Date.parse(Global.DATE_FORMAT, startDate)
-
-		when:
-		Date result = Common.addDaysToDate(start, offset)
-
-		then:
-		result.format(Global.DATE_FORMAT) == resultDate
-
-		where:
-		startDate    | resultDate   | offset
-		'01/01/2014' | '01/02/2014' | 1
-		'01/01/2014' | '02/01/2014' | 31
-		'01/01/2014' | '12/31/2013' | -1
-		'01/02/2014' | '01/01/2014' | -1
-		'01/01/2014' | '01/11/2014' | 10
-	}
-
 	void "test next weekday"() {
 		given:
 		Date startDate = Date.parse('MM/dd/yyyy', start)
