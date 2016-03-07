79a92bc87600cd2c6fe47e45ea3823eeedca8719
TheConnMan <bcconn2112@gmail.com>
2016-03-06 19:35:30 -0500

    Refactor PredictedValue send to Slack function for reuse

--- grails-app/domain/com/augurworks/engine/domains/PredictedValue.groovy
+++ grails-app/domain/com/augurworks/engine/domains/PredictedValue.groovy
@@ -28,16 +28,22 @@ class PredictedValue {
 		]
 	}
 
-	void sendToSlack() {
+	Map getSlackMap() {
 		String dateFormat = this.algorithmResult.algorithmRequest.unit == 'Day' ? Global.DATE_FORMAT : Global.DATE_TIME_FORMAT
 		String name = this.algorithmResult.algorithmRequest.dependantDataSet.name
 		String aggregation = this.algorithmResult.algorithmRequest.dependentRequestDataSet.aggregation
 		String modelType = this.algorithmResult.modelType
-		String message = 'The prediction for ' + name + ' (' + aggregation + ') on ' + this.date.format(dateFormat) + ' from ' + modelType + ' is ' + this.value.round(4)
-		String channel = Holders.config.augurworks.predictions.channel
-		String title = this.algorithmResult.algorithmRequest.stringify()
-		String link = Holders.config.grails.serverURL + '/algorithmRequest/show/' + this.algorithmResult.algorithmRequest.id
-		String color = this.value >=0 ? '#4DBD33' : '#ff4444'
-		new SlackMessage(message, channel).withBotName('Engine Predictions').withColor(color).withTitle(title).withLink(link).send()
+		return [
+			message: 'The prediction for ' + name + ' (' + aggregation + ') on ' + this.date.format(dateFormat) + ' from ' + modelType + ' is ' + this.value.round(4),
+			channel: Holders.config.augurworks.predictions.channel,
+			color: this.value >= 0 ? '#4DBD33' : '#ff4444',
+			title: this.algorithmResult.algorithmRequest.stringify(),
+			link: Holders.config.grails.serverURL + '/algorithmRequest/show/' + this.algorithmResult.algorithmRequest.id
+		]
+	}
+
+	void sendToSlack() {
+		Map slackMap = this.slackMap
+		new SlackMessage(slackMap.message, slackMap.channel).withBotName('Engine Predictions').withColor(slackMap.color).withTitle(slackMap.title).withLink(slackMap.link).send()
 	}
 }
