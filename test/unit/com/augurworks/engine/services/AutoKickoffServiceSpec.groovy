79ba4d96aae732dd6a41c784f964618dde9043d2
TheConnMan <bcconn2112@gmail.com>
2016-03-19 12:05:31 -0400

    Add auto kickoff service

---
+++ test/unit/com/augurworks/engine/services/AutoKickoffServiceSpec.groovy
@@ -0,0 +1,9 @@
+package com.augurworks.engine.services
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(AutoKickoffService)
+class AutoKickoffServiceSpec extends Specification {
+
+}
