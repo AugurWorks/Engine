996379519cb0874cc4abce129c8764cfbd71ca3a
TheConnMan <bcconn2112@gmail.com>
2016-04-03 11:12:43 -0400

    Clean up AugurWorksException

--- src/groovy/com/augurworks/engine/exceptions/AugurWorksException.groovy
+++ src/groovy/com/augurworks/engine/exceptions/AugurWorksException.groovy
@@ -2,13 +2,7 @@ package com.augurworks.engine.exceptions
 
 class AugurWorksException extends RuntimeException {
 
-	String message
-
 	AugurWorksException(String message) {
-		this.message = message
-	}
-
-	String getMessage() {
-		return this.message
+		super(message)
 	}
 }
