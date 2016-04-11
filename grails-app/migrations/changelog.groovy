0a5dd7dfbda718dea1ffee82be36a49a8e8380c6
TheConnMan <bcconn2112@gmail.com>
2016-04-10 22:27:58 -0400

    Revert "Add Spring Security UI tables"
    This reverts commit 21b6b5aeeac32d9c3b96e1413c34266f5da9f169.

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -275,8 +275,6 @@ databaseChangeLog = {
 	include file: 'aggregation-enum.groovy'
 
 	include file: 'cron-algorithms.groovy'
-	
-	include file: 'add-spring-security-ui.groovy'
 
 	include file: 'unit-enum.groovy'
 
