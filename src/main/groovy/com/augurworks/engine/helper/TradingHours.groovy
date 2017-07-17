92c396f4340cbac06c834c396055a559c7bc0e2b
TheConnMan <bcconn2112@gmail.com>
2017-07-16 22:24:38 -0400

    Add TradingHours helper class

---
+++ src/main/groovy/com/augurworks/engine/helper/TradingHours.groovy
@@ -0,0 +1,53 @@
+package com.augurworks.engine.helper
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+
+class TradingHours {
+
+    private static final DateFormat dayFormat = new SimpleDateFormat('MM/dd/yyyy')
+
+    private static final List<String> HOLIDAYS = [
+            '01/02/2017',
+            '01/16/2017',
+            '02/20/2017',
+            '04/14/2017',
+            '05/29/2017',
+            '07/04/2017',
+            '09/04/2017',
+            '11/23/2017',
+            '12/25/2017'
+    ]
+
+    private static final List<String> HALF_DAYS = [
+            '07/03/2017',
+            '11/24/2017'
+    ]
+
+    private static final Integer DAY_OPEN_MINUTES = 9 * 60 + 30
+    private static final Integer HALF_DAY_CLOSE_MINUTES = 13 * 60
+    private static final Integer DAY_CLOSE_MINUTES = 16 * 60
+
+    static Boolean isMarketOpen(Date date) {
+        return isWeekday(date) && !isHoliday(date) && (isDayDate(date) || isTradingHours(date))
+    }
+
+    private static Boolean isWeekday(Date date) {
+        Integer day = date[Calendar.DAY_OF_WEEK]
+        return day != Calendar.SATURDAY && day != Calendar.SUNDAY
+    }
+
+    private static Boolean isHoliday(Date date) {
+        return HOLIDAYS.contains(dayFormat.format(date))
+    }
+
+    private static Boolean isDayDate(Date date) {
+        Integer minutesOfDay = date[Calendar.HOUR_OF_DAY] * 60 + date[Calendar.MINUTE]
+        return minutesOfDay == 0
+    }
+
+    private static Boolean isTradingHours(Date date) {
+        Integer minutesOfDay = date[Calendar.HOUR_OF_DAY] * 60 + date[Calendar.MINUTE]
+        return minutesOfDay >= DAY_OPEN_MINUTES && (minutesOfDay <= HALF_DAY_CLOSE_MINUTES || (!HALF_DAYS.contains(dayFormat.format(date)) && minutesOfDay <= DAY_CLOSE_MINUTES))
+    }
+}
---
+++ src/test/groovy/unit/com/augurworks/engine/helper/TradingHoursSpec.groovy
@@ -0,0 +1,90 @@
+package unit.com.augurworks.engine.helper
+
+import com.augurworks.engine.helper.TradingHours
+import spock.lang.Specification
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+
+class TradingHoursSpec extends Specification {
+
+    private DateFormat dayFormat = new SimpleDateFormat('MM/dd/yyyy')
+    private DateFormat hourFormat = new SimpleDateFormat('MM/dd/yyyy HH:mm')
+
+    void "test regular days"() {
+        given:
+        Date date = dayFormat.parse(day)
+
+        when:
+        Boolean isMarketOpen = TradingHours.isMarketOpen(date)
+
+        then:
+        isMarketOpen == expected
+
+        where:
+        day          | expected
+        '01/09/2017' | true
+        '01/10/2017' | true
+        '01/11/2017' | true
+        '01/12/2017' | true
+        '01/13/2017' | true
+        '01/14/2017' | false
+        '01/15/2017' | false
+    }
+
+    void "test holiday days"() {
+        given:
+        Date date = dayFormat.parse(day)
+
+        when:
+        Boolean isMarketOpen = TradingHours.isMarketOpen(date)
+
+        then:
+        isMarketOpen == expected
+
+        where:
+        day          | expected
+        '01/02/2017' | false
+        '01/16/2017' | false
+        '12/25/2017' | false
+    }
+
+    void "test regular trading hours"() {
+        given:
+        Date date = hourFormat.parse(time)
+
+        when:
+        Boolean isMarketOpen = TradingHours.isMarketOpen(date)
+
+        then:
+        isMarketOpen == expected
+
+        where:
+        time               | expected
+        '01/09/2017 05:00' | false
+        '01/09/2017 09:30' | true
+        '01/09/2017 12:00' | true
+        '01/09/2017 16:00' | true
+        '01/09/2017 16:30' | false
+    }
+
+    void "test holiday trading hours"() {
+        given:
+        Date date = hourFormat.parse(time)
+
+        when:
+        Boolean isMarketOpen = TradingHours.isMarketOpen(date)
+
+        then:
+        isMarketOpen == expected
+
+        where:
+        time               | expected
+        '07/03/2017 05:00' | false
+        '07/03/2017 09:30' | true
+        '07/03/2017 12:00' | true
+        '07/03/2017 14:00' | false
+        '07/03/2017 16:00' | false
+        '07/03/2017 16:30' | false
+    }
+}
