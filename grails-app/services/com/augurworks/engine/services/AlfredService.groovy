2d5610bb9ad36a6a5746eef81d7bc7ab981b53b5
TheConnMan <bcconn2112@gmail.com>
2017-08-05 18:58:09 -0400

    Remove unused Alfred service method

--- grails-app/services/com/augurworks/engine/services/AlfredService.groovy
+++ grails-app/services/com/augurworks/engine/services/AlfredService.groovy
@@ -7,8 +7,6 @@ import com.augurworks.engine.domains.TrainingStat
 import com.augurworks.engine.exceptions.AugurWorksException
 import com.augurworks.engine.helper.AlfredEnvironment
 import com.augurworks.engine.helper.AlgorithmType
-import com.augurworks.engine.helper.Common
-import com.augurworks.engine.helper.Global
 import com.augurworks.engine.messaging.TrainingMessage
 import com.augurworks.engine.model.RequestValueSet
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -60,22 +58,6 @@ class AlfredService {
 			log.error('Error uploading Alfred data to S3', e)
 		}
 	}
-
-	String constructPostBody(AlgorithmRequest algorithmRequest, List<RequestValueSet> dataSets) {
-        int rowNumber = dataSets*.values*.size().max()
-        /*if (dataSets.first().values.size() != rowNumber - 1) {
-		 throw new AugurWorksException('Dependant data set not sized correctly compared to independent data sets')
-		 }*/
-        Collection<String> lines = ['net ' + (dataSets.size() - 1) + ',5', 'train 1,700,0.1,700,0.000001', 'TITLES ' + dataSets.tail()*.name.join(',')
-        ] + (0..(rowNumber - 1)).collect { int row ->
-            // TO-DO: Will not work for predictions of more than one period
-            Date date = dataSets*.values.first()[row]?.date ?: Common.calculatePredictionDate(algorithmRequest.unit, dataSets*.values.first()[row - 1].date, 1)
-            return date.format(Global.ALFRED_DATE_FORMAT) + ' ' + (dataSets.first().values[row]?.value ?: 'NULL') + ' ' + dataSets.tail()*.values.collect {
-                it[row].value
-            }.join(',')
-        }
-        return lines.join('\n')
-    }
 
     private List<RequestValueSet> getDataSets(AlgorithmRequest algorithmRequest) {
         SplineRequest splineRequest = new SplineRequest(algorithmRequest: algorithmRequest, prediction: true)
