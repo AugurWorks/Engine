9988dc6dfe18c91667e0fc88fcf8a086613a9469
TheConnMan <bcconn2112@gmail.com>
2017-08-20 19:49:35 -0400

    Request only 15 min data from TD

--- src/main/groovy/com/augurworks/engine/rest/TDClient.groovy
+++ src/main/groovy/com/augurworks/engine/rest/TDClient.groovy
@@ -3,6 +3,7 @@ package com.augurworks.engine.rest
 import com.augurworks.engine.data.SingleDataRequest
 import com.augurworks.engine.exceptions.AugurWorksException
 import com.augurworks.engine.helper.Datasource
+import com.augurworks.engine.helper.TradingHours
 import com.augurworks.engine.helper.Unit
 import com.augurworks.engine.model.DataSetValue
 import grails.converters.JSON
@@ -49,6 +50,8 @@ class TDClient extends RestClient {
 		}
 		return parsedResults.first().values.collect { Map result ->
 			return new DataSetValue(new DateTime(result.date).toDate(), result[dataRequest.dataType.name().toLowerCase()])
+		}.grep { DataSetValue dataSetValue ->
+			return dataRequest.unit == Unit.DAY ? true : TradingHours.tradingMinutesBetween(dataRequest.startDate, dataSetValue.date) % dataRequest.unit.interval == 0
 		}
 	}
 
@@ -59,7 +62,7 @@ class TDClient extends RestClient {
 			startdate: dataRequest.getOffsetStartDate().format(dateFormat),
 			enddate: dataRequest.getOffsetEndDate().format(dateFormat),
 			requestidentifiertype: 'SYMBOL',
-			intervalduration: dataRequest.unit == Unit.DAY ? '1' : dataRequest.unit.interval.toString()
+			intervalduration: dataRequest.unit == Unit.DAY ? '1' : '15'
 		]
 		return parameters
 	}
