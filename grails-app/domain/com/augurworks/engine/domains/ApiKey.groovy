ef95d7b79e92229513381973377386b5142c23e9
TheConnMan <bcconn2112@gmail.com>
2017-09-15 11:35:30 -0400

    Add API Key domain object

---
+++ grails-app/domain/com/augurworks/engine/domains/ApiKey.groovy
@@ -0,0 +1,19 @@
+package com.augurworks.engine.domains
+
+class ApiKey {
+
+    String name
+    String key = UUID.randomUUID().toString()
+    Date lastUsed
+
+    static hasMany = [products: Product]
+
+    static constraints = {
+        name unique: true
+        lastUsed nullable: true
+    }
+
+    static mapping = {
+        sort 'name'
+    }
+}
