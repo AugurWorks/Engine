5d59ff33fa99ef66d84ad0823bcb6c78e40f0713
TheConnMan <bcconn2112@gmail.com>
2017-09-25 21:49:07 -0400

    Wrap the RSS controller in a try/catch

--- grails-app/controllers/com/augurworks/engine/controllers/RssController.groovy
+++ grails-app/controllers/com/augurworks/engine/controllers/RssController.groovy
@@ -15,40 +15,45 @@ class RssController {
     ActualValueService actualValueService
 
     def index(String apiKey, String productName) {
-        ApiKey key = ApiKey.findByApiKey(apiKey)
-        Product product = Product.findByName(productName)
-        if (!key || !product) {
-            render(status: 404)
-        } else {
-            String serverUrl = grailsApplication.config.grails.serverURL
+        try {
+            ApiKey key = ApiKey.findByApiKey(apiKey)
+            Product product = Product.findByName(productName)
+            if (!key || !product) {
+                render(status: 404)
+            } else {
+                String serverUrl = grailsApplication.config.grails.serverURL
 
-            SyndFeed feed = new SyndFeedImpl()
-            feed.setFeedType('rss_2.0')
-            feed.setTitle(product.getName())
-            feed.setDescription(product.getName())
-            feed.setLink(serverUrl + '/rss/' + apiKey + '/' + productName)
+                SyndFeed feed = new SyndFeedImpl()
+                feed.setFeedType('rss_2.0')
+                feed.setTitle(product.getName())
+                feed.setDescription(product.getName())
+                feed.setLink(serverUrl + '/rss/' + apiKey + '/' + productName)
 
-            List<AlgorithmRequest> algorithmRequests = AlgorithmRequest.findAllByProduct(product)
+                List<AlgorithmRequest> algorithmRequests = AlgorithmRequest.findAllByProduct(product)
 
-            List<AlgorithmResult> algorithmResults = AlgorithmResult.findAllByAlgorithmRequestInListAndComplete(algorithmRequests, true, [max: 20, sort: 'dateCreated', order: 'desc'])
+                List<AlgorithmResult> algorithmResults = AlgorithmResult.findAllByAlgorithmRequestInListAndComplete(algorithmRequests, true, [max: 20, sort: 'dateCreated', order: 'desc'])
 
-            feed.setEntries(algorithmResults.collect { result ->
-                SyndEntry entry = new SyndEntryImpl()
-                entry.setTitle(result.algorithmRequest.name)
-                entry.setUpdatedDate(result.getDateCreated())
-                entry.setPublishedDate(result.getFutureValue().date)
-                SyndContent description = new SyndContentImpl()
-                description.setValue(actualValueService.getActual(result).get()?.toString())
-                entry.setDescription(description)
-                return entry
-            })
+                feed.setEntries(algorithmResults.collect { result ->
+                    SyndEntry entry = new SyndEntryImpl()
+                    entry.setTitle(result.algorithmRequest.name)
+                    entry.setUpdatedDate(result.getDateCreated())
+                    entry.setPublishedDate(result.getFutureValue().date)
+                    SyndContent description = new SyndContentImpl()
+                    description.setValue(actualValueService.getActual(result).get()?.toString())
+                    entry.setDescription(description)
+                    return entry
+                })
 
-            Writer writer = new StringWriter()
-            SyndFeedOutput output = new SyndFeedOutput()
-            output.output(feed, writer)
-            writer.close()
+                Writer writer = new StringWriter()
+                SyndFeedOutput output = new SyndFeedOutput()
+                output.output(feed, writer)
+                writer.close()
 
-            render(text: writer.toString(), contentType: 'text/xml')
+                render(text: writer.toString(), contentType: 'text/xml')
+            }
+        } catch (Exception e) {
+            log.error('Unable to render RSS feed', e)
+            render(status: 500)
         }
     }
 }
