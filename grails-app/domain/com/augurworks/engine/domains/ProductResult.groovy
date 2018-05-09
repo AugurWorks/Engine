4838b5a3e7b91ea8311db95691850a3f004319b4
TheConnMan <bcconn2112@gmail.com>
2018-05-09 07:15:35 -0400

    Throw an exception when diffing null values in the Product Result

--- grails-app/domain/com/augurworks/engine/domains/ProductResult.groovy
+++ grails-app/domain/com/augurworks/engine/domains/ProductResult.groovy
@@ -1,5 +1,6 @@
 package com.augurworks.engine.domains
 
+import com.augurworks.engine.exceptions.AugurWorksException
 import com.augurworks.engine.model.prediction.RuleEvaluationAction
 import org.slf4j.MDC
 
@@ -72,48 +73,53 @@ class ProductResult {
         MDC.put('product', product.id.toString())
         MDC.put('productName', product.name)
         MDC.put('productResult', id.toString())
-        if (!previousRun) {
-            log.debug('HOLD: There is no previous run')
+        try {
+            if (!previousRun) {
+                log.debug('HOLD: There is no previous run')
+                return RuleEvaluationAction.HOLD
+            }
+            if (!previousRun.previousRun) {
+                log.debug('HOLD: The previous run has no previous run')
+                return RuleEvaluationAction.HOLD
+            }
+            if (tooVolatile) {
+                log.debug('HOLD: Current run is too volatile')
+                return RuleEvaluationAction.HOLD
+            }
+            if (previousRun.tooVolatile) {
+                log.debug('HOLD: Previous run is too volatile')
+                return RuleEvaluationAction.HOLD
+            }
+            if (closeChange > product.diffUpperThreshold && previousRun.closeChange > 0 && closeResult.predictedDifference > 0) {
+                log.debug('BUY: Close change upper matched, previous run is close change is greater than zero, close diff greater than zero')
+                return RuleEvaluationAction.BUY
+            }
+            if (closeChange < product.diffLowerThreshold && previousRun.closeChange < 0 && closeResult.predictedDifference < 0) {
+                log.debug('SELL: Close change lower matched, previous run close change is less than zero, close diff less than zero')
+                return RuleEvaluationAction.SELL
+            }
+            if (allPositive && previousRun.allNegative) {
+                log.debug('HOLD: Current run is all positive, previous run is all negative')
+                return RuleEvaluationAction.HOLD
+            }
+            if (allNegative && previousRun.allPositive) {
+                log.debug('HOLD: Current run is all negative, previous run is all positive')
+                return RuleEvaluationAction.HOLD
+            }
+            if (allPositive) {
+                log.debug('HOLD: Current run is all positive, previous run was not all negative')
+                return RuleEvaluationAction.BUY
+            }
+            if (allNegative) {
+                log.debug('HOLD: Current run is all negative, previous run was not all positive')
+                return RuleEvaluationAction.SELL
+            }
+            log.debug('HOLD: No rules matched')
+            return RuleEvaluationAction.HOLD
+        } catch (Exception e) {
+            log.error('HOLD: An exception occurred', e)
             return RuleEvaluationAction.HOLD
         }
-        if (!previousRun.previousRun) {
-            log.debug('HOLD: The previous run has no previous run')
-            return RuleEvaluationAction.HOLD
-        }
-        if (tooVolatile) {
-            log.debug('HOLD: Current run is too volatile')
-            return RuleEvaluationAction.HOLD
-        }
-        if (previousRun.tooVolatile) {
-            log.debug('HOLD: Previous run is too volatile')
-            return RuleEvaluationAction.HOLD
-        }
-        if (closeChange > product.diffUpperThreshold && previousRun.closeChange > 0 && closeResult.predictedDifference > 0) {
-            log.debug('BUY: Close change upper matched, previous run is close change is greater than zero, close diff greater than zero')
-            return RuleEvaluationAction.BUY
-        }
-        if (closeChange < product.diffLowerThreshold && previousRun.closeChange < 0 && closeResult.predictedDifference < 0) {
-            log.debug('SELL: Close change lower matched, previous run close change is less than zero, close diff less than zero')
-            return RuleEvaluationAction.SELL
-        }
-        if (allPositive && previousRun.allNegative) {
-            log.debug('HOLD: Current run is all positive, previous run is all negative')
-            return RuleEvaluationAction.HOLD
-        }
-        if (allNegative && previousRun.allPositive) {
-            log.debug('HOLD: Current run is all negative, previous run is all positive')
-            return RuleEvaluationAction.HOLD
-        }
-        if (allPositive) {
-            log.debug('HOLD: Current run is all positive, previous run was not all negative')
-            return RuleEvaluationAction.BUY
-        }
-        if (allNegative) {
-            log.debug('HOLD: Current run is all negative, previous run was not all positive')
-            return RuleEvaluationAction.SELL
-        }
-        log.debug('HOLD: No rules matched')
-        return RuleEvaluationAction.HOLD
     }
 
     String getSlackChannel() {
@@ -123,7 +129,7 @@ class ProductResult {
 
     private Double getDiff(Double currentValue, Double previousValue) {
         if (!currentValue || !previousValue) {
-            return null
+            throw new AugurWorksException("Current value or previous value does not exist for this Product Result: " + this.id + ", Current Value: " + currentValue + ", Previous Value: " + previousValue)
         }
         return currentValue - previousValue
     }
